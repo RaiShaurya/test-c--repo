6fd4327f21c5717fe95ee2f5a272dc567dcfab38
Saurya Rai <arunbima40@gmail.com>
2026-06-29 22:05:23 +0530

    Updated 2.cpp solution

--- DSA/Loops/2.cpp
+++ DSA/Loops/2.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 using namespace std;
 int main(){
+    int n=10829;
+    int digsum=0;
     
+    while (n>0)
+    {
+        int lastdig = n%10;
+        digsum += lastdig;
+        n=n/10;
+    }
+    cout << "sum = " << digsum ;
+    cout << endl;
+    return 0;
+    
+
 }
